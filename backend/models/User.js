ef723510683d2f156a10cabb1ba1926df9c91cc5
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-30 09:23:35 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    mobileNumber: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    picture: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
