9aaec811b5e695430aa88a2c74a2bb78c1b3d17f
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-30 21:23:36 +0530

    updated the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import {Route, Routes, Navigate} from 'react-router-dom';
+import Main from './components/main/main';
+import Signup from './components/signup/signup';
+import Login from './components/login/login';
 
 function App() {
+  const user = localStorage.getItem("token")
   return (
-    <div className="App">
-       Hello World
-    </div>
+    <Routes>
+      {user && <Route path='/' element={<Main/>}/>}
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/' element={<Navigate replace to='/login'/>}/>
+    </Routes>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Signup from './components/signup/signup';
-import Login from './components/login/login';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Login/>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
