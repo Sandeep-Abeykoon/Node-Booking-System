de44c872ead56d7e025e40403320820d12aef99a
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-08-31 10:12:32 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,98 +1,101 @@
-const express = require ('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-require('dotenv').config();
-const User = require('../models/User');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const User = require("../models/User");
 
-const { validationResult } = require('express-validator');
-const {validateUserRegistration} = require('../middleware/validateUserRegistration');
-const { validateUserLogin } = require('../middleware/validateUserLogin');
-const errorHandler = require('../middleware/errorHandler');
+const { validationResult } = require("express-validator");
+const {
+  validateUserRegistration,
+} = require("../middleware/validateUserRegistration");
+const { validateUserLogin } = require("../middleware/validateUserLogin");
+const errorHandler = require("../middleware/errorHandler");
 
 const router = express.Router();
 
 // User Registration route
-router.post('/register', validateUserRegistration, async (req, res, next) => {
-    const errors = validationResult(req);
+router.post("/register", validateUserRegistration, async (req, res, next) => {
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    // Checking whether the user already exists
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User already registered with this username" });
     }
 
-    try {
-        // Checking whether the user already exists
-        let user = await User.findOne({ email: req.body.email });
+    // Hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        if (user) {
-            return res.status(400).json({ message: 'User already registered with this username'} );
-        }
+    // Creating a new user
+    user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      mobileNumber: req.body.mobileNumber,
+      email: req.body.email,
+      password: hashedPassword,
+      image: req.body.imageUrl,
+    });
 
-        // Hashing the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-        // Creating a new user
-        user = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            mobileNumber: req.body.mobileNumber,
-            email: req.body.email,
-            password: hashedPassword,
-            image: req.body.imageUrl
-        });
-
-        await user.save();
-        res.status(201).json({ msg: 'User registered successfully!'} );
-
-    } catch (error) {
-        next (error)   // Passing the error to the error handling middleware
-    }
+    await user.save();
+    res.status(201).json({ message: "User registered successfully!" });
+  } catch (error) {
+    next(error); // Passing the error to the error handling middleware
+  }
 });
 
-
-
 // user login route
-router.post('/login', validateUserLogin, async (req, res, next) => {
-    const errors = validationResult(req);
+router.post("/login", validateUserLogin, async (req, res, next) => {
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return(
-            res.status(400).json({ errors: errors.array() })
-        );
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    // Checking whether the user exists
+    let user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "User doesn't exist. Please check your username" });
     }
-    try {
-        // Checking whether the user exists
-        let user = await User.findOne({ email: req.body.email });
 
-        if (!user) {
-            return res.status(400).json({ message: 'User doesn\'t exist. Please check your username' });
-        }
+    // Checking the validity of the password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
 
-        // Checking the validity of the password
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-    
-        if (!validPassword) {
-            return res.status(400).json({ message: 'Password Incorrect' });
-        }
-
-        // Generating the JWT
-        const tokenPayload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(tokenPayload, process.env.JWT_KEY, (err, token) => {
-            if (err) {
-                console.error('Error signing the token', err);
-                return res. status(500).json({ msg: 'Server error' });
-            }
-            res.status(200).json({ token, message: "User logged in successfully!" });
-       });
-
-    } catch (error) {
-        next(error);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Password Incorrect" });
     }
+
+    // Generating the JWT
+    const tokenPayload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(tokenPayload, process.env.JWT_KEY, (err, token) => {
+      if (err) {
+        console.error("Error signing the token", err);
+        return res.status(500).json({ msg: "Server error" });
+      }
+      res.status(200).json({ token, message: "User logged in successfully!" });
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.use(errorHandler);
